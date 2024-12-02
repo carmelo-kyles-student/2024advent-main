fee09d530347d0260a41ecf1ae7f2e4f5f82c2f7
carmelo kyles <cakye3c@student.oaisd.org>
2024-12-02 10:36:04 -0500

    setting up a basic copyable thing that will seperate the file into an array by row

--- Program.cs
+++ Program.cs
@@ -8,42 +8,20 @@ class Program
     static void Main()
     {
         // File path
-        string filePath = "C:\\Users\\cakye3c\\Downloads\\2024advent-main\\2024advent-main\\inp.txt";
 
-        // Lists to store separated values
-        List<int> list1 = new List<int>();
-        List<int> list2 = new List<int>();
+        string filePath = "C:\\Users\\cakye3c\\OneDrive\\2024advent-main\\inp.txt";
 
-        // Read file line by line
+        // List to store reports as arrays
+        List<int[]> rows = new List<int[]>();
+
+        // Read the file and split each line into an array of integers
         foreach (var line in File.ReadLines(filePath))
         {
-            // Split each line into two parts
-            string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-
-            // Add to respective lists
-            if (parts.Length == 2)
-            {
-                list1.Add(int.Parse(parts[0]));
-                list2.Add(int.Parse(parts[1]));
-            }
+            // Split the line into integers and add to the list of rows
+            int[] row = Array.ConvertAll(line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+            rows.Add(row);
         }
    
-        Console.WriteLine("List 1 Sorted:");
-        list1.Sort();
-        Console.WriteLine(string.Join(", ",list1 ));
 
-        Console.WriteLine("\nList 2:");
-        list2.Sort();
-        Console.WriteLine(string.Join(", ", list2));
-    }
-
-    public static int[] checkIfLarger(int[] l1,int[]l2){
-        int[] l3 = new int[];
-        for (int i = 0;  i < l1.Length; i++){
-            if(l1[i] > l2[i]){
-
-            }
-        }
-        return l3;
-    }
+}
 }
