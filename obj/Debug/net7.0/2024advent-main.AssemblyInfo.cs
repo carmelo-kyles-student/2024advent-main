4875b9d0235831e6fa3820290e5b492626c3948b
mellowdramatized <118319675+mellowdramatized@users.noreply.github.com>
2024-12-03 23:50:33 -0500

    pushing cause it told me to do it

--- obj/Debug/net7.0/2024advent-main.AssemblyInfo.cs
+++ obj/Debug/net7.0/2024advent-main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2024advent-main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843527d5df1aab37864a78d14381029a071e6199")]
 [assembly: System.Reflection.AssemblyProductAttribute("2024advent-main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2024advent-main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
